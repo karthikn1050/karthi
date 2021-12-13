06305aca14f9e8251fa23ccaa95a322c0ddba136
Karthikeyan <karthikn1050@gmail.com>
2021-12-13 18:25:38 +0530

    `new`

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './App.css';
 import Vision from './components/vision';
 import Image from './components/image';
+
 function App() {
   return (
     <div>
-      
-     <Vision />
+   
+    
   <Image />
     </div>
   );
--- src/components/image.js
+++ src/components/image.js
@@ -1,39 +1,62 @@
-import React, { Component } from 'react'
-import img0 from './blank.JPG'
-import img1 from './personss.JPG'
-export default  class App extends Component {  
-    state={
-      imageSrc:'',
-      imageAlt:''
-    }
-  
-    handleChange(e){
-    
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+export default class Vision extends React.Component {
+  constructor(props) {
+    super(props);
+    this.switchImage = this.switchImage.bind(this);
+    this.switchImages =this.switchImages.bind(this);
+    this.state = {
+      currentImage: 0,
+      images: [
+        "https://i.stack.imgur.com/WeyM8.jpg",
+        "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        
+       
+      ]
+    };
+  }
+
+  switchImage() {
+    if (this.state.currentImage < this.state.images.length - 1) {
       this.setState({
-        imageSrc:'https://www.allthetests.com/quiz30/picture/pic_1387718017_1.jpg',
-        
-      })
+        currentImage: 0
+      });
+    } else {
+      this.setState({
+        currentImage: 0
+      });
     }
-    handleChanges(e){
-   
-        this.setState({
-          imageSrc:'https://i.stack.imgur.com/WeyM8.jpg',
-        
-        })
-      }
+    return this.currentImage;
+  }
   
-    render() {  
-           return (  
-    <div>  
-      <button onClick={this.handleChange}>start</button>
-      <button onClick={this.handleChanges} >stop</button>
-  
-  
-        <div class="image">  
-          <img src={this.state.imageSrc} id="img-change" alt={this.state.imageAlt}/>  
-        </div>  
-    </div>  
-           )  
-       }  
-   }  
-  
+  switchImages() {
+    if (this.state.currentImage < this.state.images.length - 1) {
+      this.setState({
+        currentImage: 1
+      });
+    } else {
+      this.setState({
+        currentImage: 1
+      });
+    }
+    return this.currentImage;
+  }
+
+
+
+  render() {
+    return (
+      <div className="slideshow-container">
+        <img style={{width:"500px",height:"500px",marginTop:"100px",marginLeft:"100px"}}
+          src={this.state.images[this.state.currentImage]}
+          alt="cleaning images"
+        />
+        <button className="btn btn-success" style={{marginLeft:"100px",width:"100px"}} onClick={this.switchImages}>Start</button>
+        <button className="btn btn-danger"style={{marginLeft:"10px",width:"100px"}} onClick={this.switchImage}>Stop</button>
+      </div>
+    );
+  }
+}
