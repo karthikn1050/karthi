28aa62f765fb0a36e30a8ab9544d98f57e14a51e
karthikn1050 <karthikn1050@gmail.com>
2021-12-15 12:10:15 +0530

    hello

--- server/server.js
+++ server/server.js
@@ -1,47 +1,82 @@
-const express = require('express')
-const app = express();
+const express = require("express");
 const cors = require("cors");
-const mysql = require('mysql')
-app.use(cors());
+const mysql = require("mysql")
+const app = express();
 
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
 
-const db = mysql.createPool({
-    host: "localhost",
-    user:"root",
-    password:"Welcome#1",
-    connectionLimit: 5,
-    database:"vision"
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+// force: true will drop the table if it already exists
+// db.sequelize.sync({force: true}).then(() => {
+//   console.log('Drop and Resync Database with { force: true }');
+//   initial();
+// });
+const dbs = mysql.createPool({
+  host: "localhost",
+  user:"root",
+  password:"Welcome#1",
+  connectionLimit: 5,
+  database:"vision"
 })
 
-db.getConnection(err => {
-    if(err){
-        throw err
-    }
-    console.log('MYSQL Connected')
+dbs.getConnection(err => {
+  if(err){
+      throw err
+  }
+  console.log('MYSQL Connected')
 })
 app.get('/items',(req,res)=>{
-    let sql ='select * from Items'
-    db.query(sql,(err,results) =>{
-        if(err){
-            throw err
-        }
-        
-        res.send(results);
-    })
+  let sql ='select * from Items'
+  dbs.query(sql,(err,results) =>{
+      if(err){
+          throw err
+      }
+      
+      res.send(results);
+  })
 })
-app.get('/status',(req,res)=>{
-    let sql ='select Status from Items'
-    db.query(sql,(err,results) =>{
-        if(err){
-            throw err
-        }
-        
-        res.send(results);
-    })
-})
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Dhvani application." });
+});
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
 
-// Set up a port
-const port = process.env.PORT || 5000;
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
 
-app.listen( port, () => console.log( `Server running on port: ${port}` ) );
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
